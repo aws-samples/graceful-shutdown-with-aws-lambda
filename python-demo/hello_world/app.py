ea5f400a479dc1cc151a3241789e300179f3ffc4
thinktik <thinktik@outlook.com>
2023-12-15 15:10:19 +0800

    1. update python3 demo for lambda graceful shutdown

--- python-demo/hello_world/app.py
+++ python-demo/hello_world/app.py
@@ -1,9 +1,16 @@
 import json
+import platform
 import signal
 import sys
 import time
 
-def exit_gracefully(signum, frame): 
+
+def exit_gracefully(signum, frame):
+    r"""
+    SIGTERM Handler: https://docs.aws.amazon.com/lambda/latest/operatorguide/static-initialization.html
+    Listening for os signals that can be handled,reference: https://docs.aws.amazon.com/lambda/latest/dg/runtimes-extensions-api.html
+    Termination Signals: https://www.gnu.org/software/libc/manual/html_node/Termination-Signals.html
+    """
     print("[runtime] SIGTERM received")
 
     print("[runtime] cleaning up")
@@ -13,14 +20,18 @@ def exit_gracefully(signum, frame):
     print("[runtime] exiting")
     sys.exit(0)
 
+
 signal.signal(signal.SIGTERM, exit_gracefully)
 
 
 def lambda_handler(event, context):
-
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "hello world",
+            "message": "hello python3",
+            "source ip": event['requestContext']['identity']['sourceIp'],
+            "architecture": platform.machine(),
+            "operating system": platform.system(),
+            "node version ": platform.python_version(),
         }),
     }
